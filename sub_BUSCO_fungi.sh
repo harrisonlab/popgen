e4df69db5763c39305be73d860facce73f1b4d5c
marynias <mkesobczyk@gmail.com>
2016-08-05 14:05:39 +0100

    Update sub_BUSCO_fungi.sh

--- sub_BUSCO_fungi.sh
+++ sub_BUSCO_fungi.sh
@@ -2,7 +2,8 @@
 #$ -cwd 
 #$ -pe smp 1
 #$ -l h_vmem=1G 
-#$ -l h=blacklace04.blacklace
+#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace11.blacklace|blacklace12.blacklace
+
 
 ### BUSCO analysis to identify single copy genes conserved in Fungi in all genomes in the study. Sample submission script for one genome:
 ### Note: use the genome mode for the transcriptome contigs, otherwise get all contigs re-named as Transcript 1 in the output!
