a23cd8d211b5150f38643c0d31242c054a35df3f
marynias <mkesobczyk@gmail.com>
2016-08-05 14:41:00 +0100

    Update sub_BUSCO_fungi.sh

--- sub_BUSCO_fungi.sh
+++ sub_BUSCO_fungi.sh
@@ -17,7 +17,7 @@ temp_dir="$TMPDIR"
 
 busco=/home/sobczm/bin/BUSCO_v1.22/BUSCO_v1.22.py
 db=/home/sobczm/bin/BUSCO_v1.22/fungi
-name=${PWD##*/} #current directory name
+name="${PWD##*/}.$(date +%F_%R)" #current directory name + timestamp
 
 ### Prep
 mkdir -p $temp_dir
