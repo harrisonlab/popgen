35cf7e85d5fbec10c76a1c0fc8c6f9f8f58ffd62
marynias <mkesobczyk@gmail.com>
2016-08-05 13:57:17 +0100

    Update sub_BUSCO_fungi.sh

--- sub_BUSCO_fungi.sh
+++ sub_BUSCO_fungi.sh
@@ -2,18 +2,31 @@
 #$ -cwd 
 #$ -pe smp 1
 #$ -l h_vmem=1G 
+#$ -l h=blacklace04.blacklace
 
 ### BUSCO analysis to identify single copy genes conserved in Fungi in all genomes in the study. Sample submission script for one genome:
 ### Note: use the genome mode for the transcriptome contigs, otherwise get all contigs re-named as Transcript 1 in the output!
+
+### Do not forget to input the path to assembly
+assembly=$1
+
+### Setting variables
+cpath=$PWD
+temp_dir="$TMPDIR"
 
 busco=/home/sobczm/bin/BUSCO_v1.22/BUSCO_v1.22.py
 db=/home/sobczm/bin/BUSCO_v1.22/fungi
 name=${PWD##*/} #current directory name
 
-## To change:
-assembly=PATH/TO/ASSEMBLY/INVESTIGATED
+### Prep
+mkdir -p $temp_dir
+cd $temp_dir
+cp $cpath/$assembly $temp_dir/$assembly
 
+### Execute
 python $busco -o $name -in $assembly -l $db -m genome
 
-
+### Cleanup
+mv $temp_dir/run_$name $cpath/run_$name
+rm -r $temp_dir
 
