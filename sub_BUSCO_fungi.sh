7a67c49f8767c9d577338c8b6215f9d9bec80fb7
marynias <mkesobczyk@gmail.com>
2016-08-08 10:22:20 +0100

    Update sub_BUSCO_fungi.sh

--- sub_BUSCO_fungi.sh
+++ sub_BUSCO_fungi.sh
@@ -5,11 +5,15 @@
 #$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace11.blacklace|blacklace12.blacklace
 
 
-### BUSCO analysis to identify single copy genes conserved in Fungi in all genomes in the study. Sample submission script for one genome:
+### BUSCO analysis to identify single copy genes conserved in Fungi in all genomes in the study. Sample submission script for one genome.
 ### Note: use the genome mode for the transcriptome contigs, otherwise get all contigs re-named as Transcript 1 in the output!
 
-### Do not forget to input the filename with the assembly (in the current dir)
+### Do not forget to input the path to the assembly 
 assembly=$1
+
+### Output folder
+filename=$(basename "$assembly")
+name="${filename%.*}_$(date +%F_%R)" #current assembly filename + timestamp for output dir
 
 ### Setting variables
 cpath=$PWD
@@ -17,12 +21,11 @@ temp_dir="$TMPDIR"
 
 busco=/home/sobczm/bin/BUSCO_v1.22/BUSCO_v1.22.py
 db=/home/sobczm/bin/BUSCO_v1.22/fungi
-name="${PWD##*/}.$(date +%F_%R)" #current directory name + timestamp for output dir
 
 ### Prep
 mkdir -p $temp_dir
 cd $temp_dir
-cp $cpath/$assembly $temp_dir/$assembly
+cp $assembly $temp_dir
 
 ### Execute
 python $busco -o $name -in $assembly -l $db -m genome
