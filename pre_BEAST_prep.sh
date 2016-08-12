40b88cc448d08965a9d13c987b6617a67ca46856
marynias <mkesobczyk@gmail.com>
2016-08-12 16:36:34 +0100

    Update pre_BEAST_prep.sh

--- pre_BEAST_prep.sh
+++ pre_BEAST_prep.sh
@@ -12,3 +12,7 @@ cp *.fasta $path/busco_alignments
 cd $path/busco_alignments
 
 for f in *.fasta; do qsub $scripts/sub_mafft_alignment.sh $f; done
+
+## Identify genes with high nucleotide diversity
+
+for f in *aligned.fasta; do python $scripts/calculate_nucleotide_diversity.py $f; done
