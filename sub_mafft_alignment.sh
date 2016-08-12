ee2063f982ed0f421223412e51784032226802a0
marynias <mkesobczyk@gmail.com>
2016-08-12 16:35:13 +0100

    Update sub_mafft_alignment.sh

--- sub_mafft_alignment.sh
+++ sub_mafft_alignment.sh
@@ -4,7 +4,6 @@
 #$ -l h_vmem=1G 
 #$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace11.blacklace|blacklace12.blacklace
 
-
 #### MAFFT alignment with the highest accuracy method L-INS-i (less than <200 sequences) 
 
 mafft_path=/home/sobczm/bin/mafft-7.222/bin/mafft
