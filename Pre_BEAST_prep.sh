30b053ecbb598061498787e2095143365d0a2126
marynias <mkesobczyk@gmail.com>
2016-08-12 16:33:27 +0100

    Create Pre_BEAST_prep.sh

---
+++ Pre_BEAST_prep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ 
+###Preparing alignments and finding best-fit nucleotide sequence evolution models
+
+path=/home/sobczm/popgen
+scripts=/home/sobczm/bin/scripts
+
+##MAFFT (alignments)
+
+cd $path/busco
+cp *.fasta $path/busco_alignments 
+cd $path/busco_alignments
+
+for f in *.fasta; do qsub $scripts/sub_mafft_alignment.sh $f; done
