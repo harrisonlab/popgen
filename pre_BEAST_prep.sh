4cfef52662b7ffd87cd04720af0d6a55d6855250
marynias <mkesobczyk@gmail.com>
2016-08-15 12:56:48 +0100

    Update pre_BEAST_prep.sh

--- pre_BEAST_prep.sh
+++ pre_BEAST_prep.sh
@@ -8,7 +8,7 @@ scripts=/home/sobczm/bin/scripts
 ##MAFFT (alignments)
 
 cd $path/busco
-cp *.fasta $path/busco_alignments 
+mv *.fasta $path/busco_alignments 
 cd $path/busco_alignments
 
 for f in *.fasta; do qsub $scripts/sub_mafft_alignment.sh $f; done
@@ -16,3 +16,39 @@ for f in *.fasta; do qsub $scripts/sub_mafft_alignment.sh $f; done
 ## Identify genes with high nucleotide diversity
 
 python $scripts/calculate_nucleotide_diversity.py "*aligned.fasta"
+
+## Visually inspect the alignments of select genes to be used in constructing the phylogenies and trim them as necessary in MEGA6.
+## Copy the relevant trimmed alignment FASTA files into $path/beast_runs
+
+##PartitionFinder (nucleotide sequence evolution model)
+
+config_template=/home/sobczm/bin/PartitionFinder1.1.1/fus.cfg
+ct=$(basename "$config_template")
+
+cd $path/beast_runs
+
+# prepare directory for PartitionFinder run:
+for f in *trimmed.fasta
+do
+c="$(cat $f | awk 'NR%2==0' | awk '{print length($1)}' | head -1)"
+p="${f%.fasta}.phy"
+dir="${f%_trimmed*}"
+
+mkdir $dir
+cp $config_template $dir
+
+# Substitute the name of the alignment file and the sequence length in the config file to become correct for the current run.
+
+sed -i 's,^\(alignment = \).*,\1'"$p;"',' $dir/$ct
+sed -i 's,^\(Gene1_pos1 = \).*,\1'"1-$c\\\3;"',' $dir/$ct
+sed -i 's,^\(Gene1_pos2 = \).*,\1'"2-$c\\\3;"',' $dir/$ct
+sed -i 's,^\(Gene1_pos3 = \).*,\1'"3-$c\\\3;"',' $dir/$ct
+
+# Convert FASTA to phylip for the run
+$scripts/fasta2phylip.pl $f>$p
+mv $p $dir 
+
+qsub $scripts/sub_partition_finder.sh $dir
+
+done
+
