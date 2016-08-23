c0c716f3b8f771d31fcde37e631b954bcd846254
marynias <mkesobczyk@gmail.com>
2016-08-23 11:47:32 +0100

    Update fus_SNP_calling.sh

--- snp/fus_SNP_calling.sh
+++ snp/fus_SNP_calling.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+input=/home/sobczm/popgen/input/mappings
+reference=Fus2_canu_contigs_unmasked.fa
+filename=$(basename "$reference")
+output="${filename%.*}.dict" 
+
+#Prepare genome reference indexes
+java -jar /home/sobczm/bin/picard-tools-2.5.0/picard.jar CreateSequenceDictionary R=$input/$reference O=$input/$output
+samtools faidx $input/$reference
+
 cd /home/sobczm/popgen/snp/snp_calling
 scripts=/home/sobczm/bin/scripts
+
 qsub $scripts/sub_fus_SNP_calling.sh
