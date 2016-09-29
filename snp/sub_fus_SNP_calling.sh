1e9e155cdd4a7fdaa536108f30835793f5dadf39
marynias <mkesobczyk@gmail.com>
2016-09-29 09:58:12 +0100

    Update sub_fus_SNP_calling.sh

--- snp/sub_fus_SNP_calling.sh
+++ snp/sub_fus_SNP_calling.sh
@@ -4,6 +4,7 @@
 #$ -l h_vmem=16G
 #$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
 
+#Note: this is a haploid organism. For diploid organism, change "ploidy" argument to 2.
 input=/home/sobczm/popgen/input/mappings
 
 reference=Fus2_canu_contigs_unmasked.fa
