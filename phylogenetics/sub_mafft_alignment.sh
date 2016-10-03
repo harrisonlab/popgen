90bf9c548539e600ddf473cde1bf99ad644ef0e5
marynias <mkesobczyk@gmail.com>
2016-10-03 10:17:45 +0100

    Update sub_mafft_alignment.sh

--- phylogenetics/sub_mafft_alignment.sh
+++ phylogenetics/sub_mafft_alignment.sh
@@ -1,6 +1,6 @@
 #$ -S /bin/bash
 #$ -cwd
-#$ -pe smp 16
+#$ -pe smp 6
 #$ -l h_vmem=1G
 #$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
 
