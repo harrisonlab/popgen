c8048d5918be9862dc646dc9deed5c335a312226
marynias <mkesobczyk@gmail.com>
2016-10-04 13:47:07 +0100

    Update fus_SNP_calling_multithreaded.sh

--- snp/fus_SNP_calling_multithreaded.sh
+++ snp/fus_SNP_calling_multithreaded.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+#To change in each analysis:
 
 input=/home/sobczm/popgen/input/mappings
 reference=Fus2_canu_contigs_unmasked.fa
+
 filename=$(basename "$reference")
 output="${filename%.*}.dict"
 
-#Prepare genome reference indexes
+#Prepare genome reference indexes required by GATK
 java -jar /home/sobczm/bin/picard-tools-2.5.0/picard.jar CreateSequenceDictionary R=$input/$reference O=$input/$output
 samtools faidx $input/$reference
 
+#Move to the directory where the output of SNP calling should be placed
 cd /home/sobczm/popgen/snp/snp_calling/multithreaded
-scripts=/home/sobczm/bin/popgen/snp
 
+#Start SNP calling with GATK
+scripts=/home/sobczm/bin/popgen/snp
 qsub $scripts/sub_fus_SNP_calling_multithreaded.sh
