e169409b45130b8d9639afc6f8ed2e45b5fd18e4
marynias <mkesobczyk@gmail.com>
2016-10-26 16:41:20 +0100

    update

--- clock/motif_discovery/clock_ortho.sh
+++ clock/motif_discovery/clock_ortho.sh
@@ -36,3 +36,33 @@ dir=OrthoFinder
 $anaconda $orthofinder/orthofinder.py -f $dir -t 16
 
 ##DATASET B)
+#Get whole-genome protein sequences and rename the files
+cd $input/pep_genomes
+mv Chaetomium_globosum_cbs_148_51.ASM14336v1.pep.all.fa Chaetomium_globosum.pep.fa
+mv Chaetomium_thermophilum_var_thermophilum_dsm_1495.CTHT_3.0.pep.all.fa Chaetomium_thermophilum.pep.fa
+mv Fusarium_fujikuroi.EF1.pep.all.fa Fusarium_fujikuroi.pep.fa
+mv Fusarium_langsethiae.ASM129263v1.pep.all.fa Fusarium_langsethiae.pep.fa
+mv Fusarium_pseudograminearum.GCA_000303195.1.pep.all.fa Fusarium_pseudograminearum.pep.fa
+mv Fusarium_verticillioides.ASM14955v1.pep.all.fa Fusarium_verticillioides.pep.fa
+mv Gaeumannomyces_graminis.Gae_graminis_V2.pep.all.fa Gaeumannomyces_graminis.pep.fa
+mv Magnaporthe_poae.Mag_poae_ATCC_64411_V1.pep.all.fa Magnaporthe_poae.pep.fa
+mv Neurospora_tetrasperma_fgsc_2508.v2.0.pep.all.fa Neurospora_tetrasperma.pep.fa
+mv Sclerotinia_borealis_f_4157.SBOR_1.pep.all.fa Sclerotinia_borealis.pep.fa
+mv Sclerotinia_sclerotiorum.ASM14694v1.pep.all.fa Sclerotinia_sclerotiorum.pep.fa
+mv Thielavia_terrestris_nrrl_8126.ASM22611v1.pep.all.fa Thielavia_terrestris.pep.fa
+mv Trichoderma_atroviride_imi_206040.TRIAT_v2_0.pep.all.fa Trichoderma_atroviride.pep.fa
+mv Trichoderma_gamsii.ASM148177v1.pep.all.fa Trichoderma_gamsii.pep.fa
+mv Trichoderma_harzianum.ASM98886v1.pep.all.fa Trichoderma_harzianum.pep.fa
+mv Trichoderma_virens.ASM17099v1.pep.all.fa Trichoderma_virens.pep.fa
+mv Verticillium_longisporum_gca_001268165.vl2.denovo.v1.pep.all.fa Verticillium_longisporum.pep.fa
+
+#Create a directory for OrthoFinder run, copy input FASTA files and run orthofinder
+mkdir OrthoFinder2
+for a in *fa; do cp $a ./OrthoFinder2; done
+# Running a dummy script to reserve resources on blacklace01
+qsub /home/sobczm/bin/popgen/other/reserve_cluster.sh
+#Running orthofinder on the directory OrthoFinder containing all input files
+orthofinder=/home/sobczm/bin/OrthoFinder-1.0.7/orthofinder
+anaconda=/home/sobczm/bin/anaconda2/bin/python
+dir=OrthoFinder2
+$anaconda $orthofinder/orthofinder.py -f $dir -t 16
