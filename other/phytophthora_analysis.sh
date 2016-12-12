d6160a9980835028553733bbdbc3c42079657d76
marynias <mkesobczyk@gmail.com>
2016-12-12 17:28:28 +0000

    update

--- other/phytophthora_analysis.sh
+++ other/phytophthora_analysis.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 scripts=/home/sobczm/bin/popgen/other
 input=/home/sobczm/popgen/other/phytophthora
+
+#Get OrthoMCL results into a more readable tabular format
+python $scripts/rearrange_orthomcl_results_with_rubi.py All_Strains_plus_rubi_orthogroups.txt
+python $scripts/rearrange_orthomcl_results_without_rubi.py All_Strains_orthogroups.txt
+#How many orthogroups present in all strains but BC16?
+Orthogroups=All_Strains_orthogroups.txt
+cat $Orthogroups | grep 'A4|' | grep 'Bc1|' | grep 'Bc23|' | grep 'Nov27|' | grep 'Nov5|' | grep 'Nov71|' | grep 'Nov77|' | grep 'Nov9|' | grep 'ONT3|' | grep 'SCRP245_v2|' | grep -v 'Bc16|' >no_Bc16_groups.txt
+#There appears to be 230 such orthogroups, out of 19952.
---
+++ other/rearrange_orthomcl_results_without_rubi.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+from sys import argv
+from collections import defaultdict as dd
+import os, sys, re
+
+#Split the OrthoMCL output file with the output orthgroups into the
+#same format as OrthoFinder.
+
+#Specify an array containing the genome names in the order desired.
+genomes = ["A4", "Bc1", "Bc16", "Bc23", "Nov27", "Nov5", "Nov71", "Nov77", "Nov9", "ONT3", "SCRP245_v2"]
+script, orthomcl_file = argv
+
+sum_out = open("orthomcl_output.tsv", 'w')
+sum_out.write("Orthogroup" + "\t" + "\t".join(genomes) + "\n")
+split_id_gene = r"(\w+)(\|)(.+)"
+
+orthomcl_h = open(orthomcl_file)
+for line in orthomcl_h:
+    fields = line.split()
+    sum_out.write(fields[0] + "\t")
+    lr = dd(list)
+    lr.clear()
+    for record in fields[1:]:
+        result = re.search(split_id_gene, record)
+        lr[result.group(1)].append(result.group(3))
+    for g in genomes:
+        sum_out.write(",".join(lr[g]))
+        sum_out.write("\t")
+    sum_out.write("\n")
+
+sum_out.close()
+orthomcl_h.close()
