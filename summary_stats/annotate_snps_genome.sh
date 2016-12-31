e552333c02daf7ab36c72d167fb814b4a6973fc0
marynias <mkesobczyk@gmail.com>
2016-12-31 11:32:06 +0000

    Update annotate_snps_genome.sh

--- summary_stats/annotate_snps_genome.sh
+++ summary_stats/annotate_snps_genome.sh
@@ -11,7 +11,8 @@
 #(see build_genome_database.sh script)
 
 #Output:
-#Various VCF files with suffix "_annotated"
+#Various VCF files with suffix "_annotated": all, genic, coding, non-synonymous, synonymous and four-fold degenerate sites
+#A HTML file with  detailed summary statistics on SNPs detected
 
 vcf=$1
 genome_name=$2
@@ -19,20 +20,19 @@ genome_name=$2
 snpeff=/home/sobczm/bin/snpEff
 scripts=/home/sobczm/bin/popgen
 
-filename=$(basename "$vcf")
-java -Xmx4g -jar $snpeff/snpEff.jar -v -ud 0 $genome_name $vcf > ${filename%.vcf}_annotated.vcf
-mv snpEff_genes.txt snpEff_genes_${filename%.vcf}.txt
-mv snpEff_summary.html  snpEff_summary__${filename%.vcf}.html
+java -Xmx4g -jar $snpeff/snpEff.jar -v -ud 0 $genome_name $vcf > ${vcf%.vcf}_annotated.vcf
+mv snpEff_genes.txt snpEff_genes_${vcf%.vcf}.txt
+mv snpEff_summary.html  snpEff_summary_${vcf%.vcf}.html
 
 #Create subsamples of SNPs containing those in a given category
 
 #genic (includes 5', 3' UTRs)
-java -jar $snpeff/SnpSift.jar filter "(ANN[*].EFFECT has 'missense_variant') || (ANN[*].EFFECT has 'nonsense_variant') || (ANN[*].EFFECT has 'synonymous_variant') || (ANN[*].EFFECT has 'intron_variant') || (ANN[*].EFFECT has '5_prime_UTR_variant') || (ANN[*].EFFECT has '3_prime_UTR_variant')" ${filename%.vcf}_annotated.vcf > ${filename%.vcf}_gene.vcf
+java -jar $snpeff/SnpSift.jar filter "(ANN[*].EFFECT has 'missense_variant') || (ANN[*].EFFECT has 'nonsense_variant') || (ANN[*].EFFECT has 'synonymous_variant') || (ANN[*].EFFECT has 'intron_variant') || (ANN[*].EFFECT has '5_prime_UTR_variant') || (ANN[*].EFFECT has '3_prime_UTR_variant')" ${vcf%.vcf}_annotated.vcf > ${vcf%.vcf}_gene.vcf
 #coding
-java -jar $snpeff/SnpSift.jar filter "(ANN[0].EFFECT has 'missense_variant') || (ANN[0].EFFECT has 'nonsense_variant') || (ANN[0].EFFECT has 'synonymous_variant')" ${filename%.vcf}_annotated.vcf > ${filename%.vcf}_coding.vcf
+java -jar $snpeff/SnpSift.jar filter "(ANN[0].EFFECT has 'missense_variant') || (ANN[0].EFFECT has 'nonsense_variant') || (ANN[0].EFFECT has 'synonymous_variant')" ${vcf%.vcf}_annotated.vcf > ${vcf%.vcf}_coding.vcf
 #non-synonymous
-java -jar $snpeff/SnpSift.jar filter "(ANN[0].EFFECT has 'missense_variant') || (ANN[0].EFFECT has 'nonsense_variant')" ${filename%.vcf}_annotated.vcf > ${filename%.vcf}_nonsyn.vcf
+java -jar $snpeff/SnpSift.jar filter "(ANN[0].EFFECT has 'missense_variant') || (ANN[0].EFFECT has 'nonsense_variant')" ${vcf%.vcf}_annotated.vcf > ${vcf%.vcf}_nonsyn.vcf
 #synonymous
-java -jar $snpeff/SnpSift.jar filter "(ANN[0].EFFECT has 'synonymous_variant')" ${filename%.vcf}_annotated.vcf > ${filename%.vcf}_syn.vcf
+java -jar $snpeff/SnpSift.jar filter "(ANN[0].EFFECT has 'synonymous_variant')" ${vcf%.vcf}_annotated.vcf > ${vcf%.vcf}_syn.vcf
 #Four-fold degenrate sites (output file suffix: 4fd)
-python $scripts/summary_stats/parse_snpeff_synonymous.py ${filename%.vcf}_syn.vcf
+python $scripts/summary_stats/parse_snpeff_synonymous.py ${vcf%.vcf}_syn.vcf
