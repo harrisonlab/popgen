2c4b8b5a3204c5672d57023bc7d7a352c6a4dfb8
marynias <mkesobczyk@gmail.com>
2017-01-04 15:15:34 +0000

    Update calculate_nucleotide_diversity.R

--- summary_stats/calculate_nucleotide_diversity.R
+++ summary_stats/calculate_nucleotide_diversity.R
@@ -6,9 +6,13 @@ library(ggplot2)
 #of genetic variation cannot be calculated, and need to carry out only analyses
 #A, B  but not C, D.
 #When all coding sites input, use E to calculate Pi(nonsyn)/Pi(syn) sites.
+
+##!!!!!!!  DIPLOID organisms
+##When using diploid organisms and input FASTA files generated using vcf_to_fasta.py, each sample will be artificially
+##split into two sequences (<sample_name> + prefix (_1 or _2), for example FOC5_1, FOC5_2), each representing
+##one haplotype. Both need to be input below.
 nonpatho <- c("FOCA1-2", "FOCA28", "FOCCB3", "FOCD2", "FOCHB6", "FOCPG")
 patho <- c("FOCA23", "FOC55", "FOC125", "FOCFus2")
-#Need to set argument diploid=TRUE if using diploid genomes in the below command:
 populations <- list(nonpatho, patho)
 #Number of populations assigned above.
 population_no <- length(populations)
