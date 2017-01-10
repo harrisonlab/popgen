e41f812c92e0df257aeeba12e490efad77223b9b
marynias <mkesobczyk@gmail.com>
2017-01-10 15:37:08 +0000

    update

--- renseq/identification_of_atypical_genes.sh
+++ renseq/identification_of_atypical_genes.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+#Looking for a subset of RLKs: SRLK (S-receptor like kinase genes)
+
+############Xing2013
+#B_lectin(PF01453, IPR001480)-SLG(S_locus_glycop: PF00954, IPR000858)-PAN_APPLE (PF00024, PF14295, PF08276, SM00473, IPR003609)-TM-Kinase domain
+############Chen2006 and Catanzariti2015
+#SLG missing
+#Just any lectin? -> keyword search
+#Pan or
+#TM-kinase domain
+
+####First, identify kinase-containing genes.
+
+
+
+
+
+
+
+#####Lastly, run Phobius on selected sequences
+
+
+
+
+####Bait design
