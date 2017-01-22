3ac3ecd2d01528a80aaf3d342928a6af752828dc
marynias <mkesobczyk@gmail.com>
2017-01-22 12:20:13 +0000

    Update calculate_outgroup_faywuh.R

--- summary_stats/calculate_outgroup_faywuh.R
+++ summary_stats/calculate_outgroup_faywuh.R
@@ -31,9 +31,7 @@ for (dir in contig_folders[contig_folders != ""])
   GENOME.class <- readData(contig_folder, gffpath=gff, include.unknown = TRUE)
   GENOME.class <- set.populations(GENOME.class, populations)
   GENOME.class <- set.outgroup(GENOME.class, new.outgroup=ancestral)
-  
-  ###############################################################################
-  
+
   #Calculate neutrality stats over genes
   GENOME.class.split <- splitting.data(GENOME.class, subsites="gene")
   GENOME.class.split <- neutrality.stats(GENOME.class.split)
