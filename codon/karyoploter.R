d5590b15bec285f8f7c4e3f0ef73fb31125bf6bd
marynias <mkesobczyk@gmail.com>
2017-01-31 12:58:12 +0000

    Update karyoploter.R

--- codon/karyoploter.R
+++ codon/karyoploter.R
@@ -9,14 +9,28 @@ all.genes <- read.delim(paste("all_genes/",cname, sep=""), header=TRUE)
 segmental <- read.delim(paste("segmental/",cname, sep=""), header=TRUE)
 tandem <- read.delim(paste("tandem/",cname, sep=""), header=TRUE)
 
-
 pdf(paste(cname, ".pdf", sep=""), width=6.95, height=4.27)
 kp <- plotKaryotype(genome = custom.genome, plot.type=2, labels.plotter=NULL)
 kpDataBackground(kp, data.panel = 1, r0=0, r1=0.8, color="lavenderblush")
-kpDataBackground(kp, data.panel = 2, r0=0, r1=0.7, color="aliceblue")
-l <- seq(0, genome_df$end, by = genome_df$end/10)
-lab <- pretty(l, n=10)
-axis(1, at=lab/genome_df$end, labels=format(lab, scientific=FALSE), cex.axis=0.7)
+kpDataBackground(kp, data.panel = 2, r0=0.05, r1=0.7, color="aliceblue")
+#Vary x scale according to the contig size. 
+#For small contigs, smaller than 1 million bp (contigs 15 and above), minor tick every 10,000 bp
+#For big contigs, minor tick every 100,000 bp
+if (contig > 14) {
+  minor_tick=10000
+  major_tick=1000000
+}
+else {
+  minor_tick=100000
+  major_tick=1000000
+}
+
+kpAddBaseNumbers(kp, tick.dist = major_tick, tick.len = 10, tick.col="gray", col.lab="gray", cex=0.6,
+                 minor.tick.dist = minor_tick, minor.tick.len = 5, minor.tick.col = "gray")
+#Custom x axis - eliminiated due to addition of kpAddBaseNumbers function
+#l <- seq(0, genome_df$end, by = genome_df$end/10)
+#lab <- pretty(l, n=10)
+#axis(1, at=lab/genome_df$end, labels=format(lab, scientific=FALSE), cex.axis=0.7)
 
 
 title(main=paste("contig", contig), col.main="gray65")
@@ -80,7 +94,7 @@ segmental_input <- cbind(cc, segmental_input)
 #plot segmental duplications
 
 #kpAxis(kp, ymin=0, ymax=max(all_genes_input$obs_all), r0=0, r1=0.8, col="gray50", cex=0.5, numticks=max(all_genes_input$obs_all)+1)
-#Fixed at 10
+#Fixed at 10 genes
 ymaximum=10
 kpAxis(kp, ymin=0, ymax=ymaximum, r0=0, r1=0.8, col="gray50", cex=0.5, numticks=11)
 
@@ -131,9 +145,7 @@ six <- read.delim(paste("six/",cname, sep=""), header=TRUE)
 y <- rep(1,length(six$chr))
 input_six <- cbind(six, y)
 kpPoints(kp, chr=input_six$chr, x=input_six$start, y=input_six$y,
-         col="black", ymin=0, ymax=1, r0=0.6, r1=0.7, data.panel = 2, pch=".", cex=5.5)
+         col="black", ymin=0, ymax=1, r0=0.6, r1=0.66, data.panel = 2, pch=".", cex=5.5)
 
 dev.off()
 }
-
-
