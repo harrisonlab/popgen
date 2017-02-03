b8559a4e9f487ee8d27fa36fe50141718e491034
marynias <mkesobczyk@gmail.com>
2017-02-03 00:30:48 +0000

    update

--- summary_stats/sub_calculate_4gt.sh
+++ summary_stats/sub_calculate_4gt.sh
@@ -4,6 +4,6 @@
 #$ -l h_vmem=8G
 #$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
 
-scripts=/home/sobczm/bin/popgen/other
+scripts=/home/sobczm/bin/popgen/summary_stats
 
 Rscript --vanilla $scripts/calculate_4gt.R
