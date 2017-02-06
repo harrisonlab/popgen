dcf9751b378cce2737e2581ca25bf606210a71e9
marynias <mkesobczyk@gmail.com>
2017-02-06 14:13:38 +0000

    update

---
+++ summary_stats/ld_plot.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+#Plots r^2 (measure of linkage disequilibrium) as a heatmap.
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)}
+
+library(gg.ldplot)
+table <- args[1]
+vcf_ld <- read.table(table, header = TRUE, stringsAsFactors = F)
+contig_list <- split(vcf_ld, vcf_ld$CHR)
+
+#Print R2 LD plot for each contig
+for (x in 1:length(contig_list))
+{
+filename <- paste(contig_list[[x]][1,1], "_ld_plot.pdf", sep="")
+pdf(filename, width=7, height=5)
+print(plot_LDmatrix(contig_list[[x]]))
+dev.off()
+}
