8df81cd22ea2a0d06b3e184d7c8bfd985efb69b1
marynias <mkesobczyk@gmail.com>
2017-07-03 10:50:31 +0100

    update

---
+++ genome/README.md
@@ -0,0 +1,1 @@
+1) [genome_browsers.sh] (https://github.com/harrisonlab/popgen/blob/master/genome/genome_browsers.sh)
---
+++ genome/genome_browsers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+input=/home/sobczm/popgen/genome
+scripts=/home/sobczm/bin/popgen/genome
+
+#Install UCSC, GBrowse and Ensembl genome browsers for ananassa latest genome.
+
+#Copy input genome. (Latest D. Swarbreck annotation)
+cp -r /home/sobczm/popgen/rnaseq/ananassa_annotation $input
