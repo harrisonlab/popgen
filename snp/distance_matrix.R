f91796b162a3d78f2dd2ffed68d8625cc2d15084
marynias <mkesobczyk@gmail.com>
2017-07-05 16:03:32 +0100

    Update distance_matrix.R

--- snp/distance_matrix.R
+++ snp/distance_matrix.R
@@ -2,17 +2,30 @@
 
 #Print a heatmap displaynig the distance results output by similarity_percentage.py
 #First argument (required): input distance results
-#Second argument (optional): output filename
+#Second argument (optional) - output figure width
+#Third argument (optional) - output figure height
+
+###Note for usage:
+##This script takes a bit of work to produce the plot matching the size of your dataset. 
+##The following parameters inside the script may have to be changed:
+#Margins - Specify the margins size before running the heatmap.2 command
+# par(mar=c(9,6,6,4)+0.1) 
+#cexRow - Row label size
+#cexCol - Column label size
+#notecex - Size of values within individual cells
+#INTeger in colorRampPalette (mypalette) (INT) - depending on the range of the values of % match, may want to increase or decrease number of colors in the scale as specified here.
+
 library(tools)
 args = commandArgs(trailingOnly=TRUE)
-
-
 if (length(args)==0) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 } else if (length(args)==1) {
   # default output file
-  args[2] = paste(file_path_sans_ext(args[1]), "_distance_matrix.pdf", sep="")
+  args[2] = 16
+  args[3] = 8
 }
+
+output <- paste(file_path_sans_ext(args[1]), "_distance_matrix.pdf", sep="")
 
 library(gplots)
 library(RColorBrewer)
@@ -21,9 +34,8 @@ distance = read.table(args[1], header=T, stringsAsFactors=F)
 mypalette<-brewer.pal(9, "YlGn")
 #Ramp up the color palette
 cols <- colorRampPalette (mypalette) (100)
-par(mar=c(9,6,6,4)+0.1) 
-pdf(args[2], width=200, height=130)
+pdf(output, args[2], args[3])
 heatmap.2(as.matrix(distance),dendrogram="col", cellnote=as.matrix(distance),
-          notecol="black",col=cols,scale="none",key=TRUE, keysize=0.25,
-          density.info="none", cexRow=2.0,cexCol=2.0, notecex=2.0, trace="none",margins=c(30,30))
+          notecol="black",col=cols,scale="none",key=TRUE, keysize=0.75,
+          density.info="none", trace="none", cexRow=1.0, cexCol=1.0, notecex=2.0)
 dev.off()
