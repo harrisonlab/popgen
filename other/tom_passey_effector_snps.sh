69899301622c3cb489209a673e67b1d3d8fcc274
marynias <mkesobczyk@gmail.com>
2017-08-21 16:22:32 +0100

    update

--- other/tom_passey_effector_snps.sh
+++ other/tom_passey_effector_snps.sh
@@ -15,11 +15,13 @@ awk '$3=="gene"' $input/Maria/all_genes/final_genes_appended.gff3 >$input/Maria/
 awk '$3=="gene"' $input/Maria/cazy/172_pacbio_CAZY.gff >$input/Maria/cazy/172_pacbio_CAZY_gene.gff 
 awk '$3=="gene"' $input/Maria/cazy_secreted/172_pacbio_CAZY_secreted.gff >$input/Maria/cazy_secreted/172_pacbio_CAZY_secreted_gene.gff
 awk '$3=="gene"' $input/Maria/metabolite_cluster/metabolite_cluster_genes.gff >$input/Maria/metabolite_cluster/metabolite_cluster_genes_gene.gff 
+awk '$3=="gene"' $input/Maria/effectorp_secreted/v.inaequalis_172_pacbio_EffectorP_secreted.gff >$input/Maria/effectorp_secreted/v.inaequalis_172_pacbio_EffectorP_secreted_gene.gff 
 #Simple intersect of different classes of genes with fixed SNPs and SVs.
 #
 for vcf_file in $input/Maria/vcf_files/Ash_farm_172_pacbio_contigs_unmasked_3_bw_filtered_fixed_gene.vcf $input/Maria/vcf_files/Ash_farm_172_pacbio_contigs_unmasked_3_bw_filtered_fixed_coding.vcf $input/Maria/vcf_files/Ash_farm_172_pacbio_contigs_unmasked_3_bw_filtered_fixed_nonsyn.vcf $input/Maria/vcf_files/Ash_farm_struc_variants_fixed.vcf  
 do
 for gff_file in $input/Maria/all_genes/final_genes_appended_gene.gff $input/Maria/cazy/172_pacbio_CAZY_gene.gff $input/Maria/cazy_secreted/172_pacbio_CAZY_secreted_gene.gff $input/Maria/metabolite_cluster/metabolite_cluster_genes_gene.gff $input/Maria/smurf_cluster/Smurf_clusters.gff 
+$input/Maria/effectorp_secreted/v.inaequalis_172_pacbio_EffectorP_secreted_gene.gff 
 do
 vcf=$(basename $vcf_file)
 intersectBed -wb -a $vcf_file -b $gff_file > ${gff_file}_${vcf%.vcf}_overlap
