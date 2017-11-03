fe8dbe98c8f6131868d2fd4bae06d8303afc075f
marynias <mkesobczyk@gmail.com>
2017-11-03 12:50:11 +0000

    update

--- snp/sub_ananassa_genotypes_db.sh
+++ snp/sub_ananassa_genotypes_db.sh
@@ -2,7 +2,7 @@
 #$ -cwd
 #$ -pe smp 1
 #$ -l h_vmem=16G
-#$ -l h=blacklace11.blacklace
+#$ -l h=blacklace03.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
 input=$1
 output=$2
 
--- snp/sub_ananassa_genotypes_vcf.sh
+++ snp/sub_ananassa_genotypes_vcf.sh
@@ -1,7 +1,7 @@
 #$ -S /bin/bash
 #$ -cwd
 #$ -pe smp 1
-#$ -l h_vmem=8G
+#$ -l h_vmem=4G
 #$ -l h=blacklace03.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
 input1=$1
 input2=$2
