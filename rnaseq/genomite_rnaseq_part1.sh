63853fe45ea679250d2a8626377f17b637f715e2
marynias <mkesobczyk@gmail.com>
2017-11-14 10:19:58 +0000

    update

--- rnaseq/genomite_rnaseq_part1.sh
+++ rnaseq/genomite_rnaseq_part1.sh
@@ -3,3 +3,6 @@ input=/home/sobczm/popgen/rnaseq/genomite
 scripts=/home/sobczm/bin/popgen/rnaseq
 
 original_data=/data/seq_data/external/20171110_genomite_rnaseq_cnag
+
+#Mite genome and gff annotation obtained from V. Zhurov.
+#Downloading vesca ver 2.0 genome and annotation ver. 
