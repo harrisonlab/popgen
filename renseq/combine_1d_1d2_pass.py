52bc673b821ef6415d74e79bce9da5955870bf4d
marynias <mkesobczyk@gmail.com>
2017-12-07 18:37:39 +0000

    update

--- renseq/combine_1d_1d2_pass.py
+++ renseq/combine_1d_1d2_pass.py
@@ -16,6 +16,7 @@ with open (one_d2) as one_d2_h:
         my_true_id = line.strip()
         keep_reads[my_id] = [sequence, plus_sign, scores, my_true_id]
 
+seen = dd()
 with open (one_d) as one_d_h:
     for line in one_d_h:
         lines = line.strip().split()
@@ -29,8 +30,20 @@ with open (one_d) as one_d_h:
             print keep_reads[my_id][0]
             print keep_reads[my_id][1]
             print keep_reads[my_id][2]
+            seen[my_id] = 1
         else:
             print my_true_id
             print sequence
             print plus_sign
             print scores
+
+all_1d2 = keep_reads.keys()
+
+for my_id in all_1d2:
+    if my_id in seen:
+        pass
+    else:
+        print keep_reads[my_id][3]
+        print keep_reads[my_id][0]
+        print keep_reads[my_id][1]
+        print keep_reads[my_id][2]
