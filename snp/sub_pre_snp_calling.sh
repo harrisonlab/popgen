fa583f964364fbb90753c81910258408a3a7700b
adarmitage <Andrew.Armitage@EMR.ac.uk>
2018-03-26 09:43:06 +0100

    if input file is in bam format comvert it to sam format within script

--- snp/sub_pre_snp_calling.sh
+++ snp/sub_pre_snp_calling.sh
@@ -28,36 +28,44 @@ mkdir -p $temp_dir
 cp $input_sam $temp_dir
 cd $temp_dir
 
+### If the provided file is a .bam file then convert it into sam format.
+
+extension=$(echo $filename | rev | cut -f1 -d '.' | rev)
+if [[ $Extension == "bam" ]]; then
+  samtools view -o $name.sam $filename
+  filename=$name.sam
+fi
+
 ### Get rid of multimapping reads by filtering out on the XS:i: tag
 grep -v "XS:i" $filename >temp && mv temp $filename
 samtools view -bS -o $name.bam $filename
-samtools sort $name.bam $name\_nomulti\_sorted
-samtools index $name\_nomulti\_sorted.bam
+samtools sort $name.bam ${name}_nomulti_sorted
+samtools index ${name}_nomulti_sorted.bam
 
 ### Keep only reads with "paired reads" and "properly paired reads" flags.
-samtools view -b -h -f 3 -o $name\_nomulti\_proper.bam $name\_nomulti\_sorted.bam
+samtools view -b -h -f 3 -o ${name}_nomulti_proper.bam ${name}_nomulti_sorted.bam
 ### Sort for downstream analyses
-samtools sort $name\_nomulti\_proper.bam $name\_nomulti\_proper\_sorted
-samtools index $name\_nomulti\_proper\_sorted.bam
+samtools sort ${name}_nomulti_proper.bam ${name}_nomulti_proper_sorted
+samtools index ${name}_nomulti_proper_sorted.bam
 
 ### Remove PCR and optical duplicates
 picard=/home/sobczm/bin/picard-tools-2.5.0/picard.jar
 java -jar $picard MarkDuplicates \
-INPUT=$name\_nomulti\_proper\_sorted.bam \
-OUTPUT=$name\_nomulti\_proper\_sorted\_nodup.bam \
-METRICS_FILE=$name\_nomulti\_proper\_sorted\_nodup.txt \
+INPUT=${name}_nomulti_proper_sorted.bam
+OUTPUT=${name}_nomulti_proper_sorted_nodup.bam \
+METRICS_FILE=${name}_nomulti_proper_sorted_nodup.txt \
 REMOVE_DUPLICATES=TRUE ASSUME_SORTED=TRUE MAX_RECORDS_IN_RAM=500000000 \
 MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000 VALIDATION_STRINGENCY=LENIENT
 ### Add group and sample name (prefix)
 java -jar $picard AddOrReplaceReadGroups \
-INPUT=$name\_nomulti\_proper\_sorted\_nodup.bam \
-OUTPUT=$name\_nomulti\_proper\_sorted\_nodup_rg.bam \
+INPUT=${name}_nomulti_proper_sorted_nodup.bam \
+OUTPUT=${name}_nomulti_proper_sorted_nodup_rg.bam \
 SORT_ORDER=coordinate CREATE_INDEX=true RGID=$prefix  RGSM=$prefix \
 RGPL=Illumina RGLB=library RGPU=barcode VALIDATION_STRINGENCY=LENIENT
-samtools index $name\_nomulti\_proper\_sorted\_nodup_rg.bam
+samtools index ${name}_nomulti_proper_sorted_nodup_rg.bam
 
 ### Cleanup
-mv $name\_nomulti\_proper\_sorted\_nodup.txt $cpath
-mv $name\_nomulti\_proper\_sorted\_nodup_rg.bam $cpath
-mv $name\_nomulti\_proper\_sorted\_nodup_rg.bam.bai $cpath
+mv ${name}_nomulti_proper_sorted_nodup.txt $cpath
+mv ${name}_nomulti_proper_sorted_nodup_rg.bam $cpath
+mv ${name}_nomulti_proper_sorted_nodup_rg.bam.bai $cpath
 rm -rf $temp_dir
